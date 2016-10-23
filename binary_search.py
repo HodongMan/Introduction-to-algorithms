b79c85de5e346dc77fc8bb5c19cb5b4738915381
HodongMan <arshavin3@naver.com>
2016-10-23 11:50:41 +0900

    binary tree complete

--- binary_search.py
+++ binary_search.py
@@ -47,8 +47,8 @@ class BinaryTree(object):
 	def InorderTreeWalk(self, x):
 		if x is not None:
 			self.InorderTreeWalk(x.left)
-			print(x)
-			self.InroderTreeWalk(x.right)
+			print(x.data)
+			self.InorderTreeWalk(x.right)
 		
 	def InorderTreeStart(self):
 		self.InorderTreeWalk(self._root)
@@ -78,8 +78,62 @@ class BinaryTree(object):
 		while x.right is not None:
 			x = x.right
 		return x
+	
+	def TreeSuccessor(self, x):
+		if x.right is not None:
+			return self.TreeMinimum(x.right)
+		y = x.parent
+		while y is not None and x == y.right:
+			x = y
+			y = y.parent
+		return y
+
+	def TreeInsert(self, z):
+		y = None
+		x = self._root
+		while x is not None:
+			y = x
+			if z.data < x.data:
+				x = x.left
+			else:
+				x = x.right
+		z.p = y
+		if y == None:
+			self._root = z
+		elif z.data < y.data:
+			y.left = z
+		else:
+			y.right = z
+	
+	def Translate(self, x, y):
+		if x.parent is None:
+			self._root = y
+		elif x == x.parent.left:
+			x.parent.left = y
+		else:
+			x.parent.right = y
+		if y is not None:
+			y.parent = x.parent
+
+	def TreeDelete(self, z):
+		if z.left is None:
+			self.Translate(z, z.right)
+		elif z.right is None:
+			self.Translate(z, z.left)
+		else:
+			y = self.TreeMinimum(z.right)
+			if y.p is not z:
+				self.Translate(y, y.right)
+				y.right = z.right
+				y.right.paret = y
+			self.Translate(z, y)
+			y.left = z.left
+			y.left.parent = y
 
 
 
 if __name__ == "__main__":
 	bt = BinaryTree()
+	bt.TreeInsert(Node(1))
+	bt.TreeInsert(Node(2))
+	bt.InorderTreeStart()
