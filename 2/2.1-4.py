cd9b1d1b0f2f1b38c264373bf65deb5f172f5245
hodongman <jhd9206@gmail.com>
2018-10-23 01:56:13 +0900

    2.1.4 python update

--- 2/2.1-4.py
+++ 2/2.1-4.py
@@ -4,16 +4,14 @@ class BinaryAdd(object):
 		self._result = list()
 	
 	def add(self, list1, list2):
-		self._result = [0]
+		carry = 0
 
 		for index, item in enumerate(list1):
-			self._result.append(item + list2[index])
+			self._result.append( ( item + list2[index] + carry ) % 2 )
+			carry = ( item + list2[index] + carry ) // 2
+			print(self._result[index], carry)
 
-		for i in range(len(self._result) -1, 0, -1):
-
-			if self._result[i] > 1:
-				self._result[i] -= 2
-				self._result[i-1] += 1
+		self._result.append(carry)
 
 	def printData(self):
 		print(self._result)
